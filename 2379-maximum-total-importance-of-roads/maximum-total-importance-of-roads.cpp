807cbedeccbe3bc3300090525992397ec48b7b3e
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2024-06-28 13:32:51 +0530

    Time: 291 ms (63.45%) | Memory: 132.6 MB (60.69%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -12,39 +12,50 @@ public:
             }
         }
 
-        vector<vector<int>> v;
-
-        for(int i=0;i<n;i++)
-        {
-            v.push_back({o_d[i],i});
-        }
-
-        sort(v.begin(),v.end());
-
-        for(int i=0;i<n;i++)
-        {
-            v[i][0] = i+1;
-            swap(v[i][0],v[i][1]);
-            // cout<<v[i][0]<<" "<<v[i][1]<<endl;
-        }
-
-        sort(v.begin(),v.end());
+        sort(o_d.begin(),o_d.end());
 
         long long ans = 0;
 
-        for(auto i:roads)
+        for(long long i=0;i<n;i++)
         {
-            long long x = 0;
-
-            for(auto j:i)
-            {
-                x += v[j][1];
-            }
-
-            // cout<<x<<endl;
-            ans += x;
+            ans += ((i+1)*o_d[i]);
         }
 
         return ans;
+
+        // vector<vector<int>> v;
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     v.push_back({o_d[i],i});
+        // }
+
+        // sort(v.begin(),v.end());
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     v[i][0] = i+1;
+        //     swap(v[i][0],v[i][1]);
+        //     // cout<<v[i][0]<<" "<<v[i][1]<<endl;
+        // }
+
+        // sort(v.begin(),v.end());
+
+        // long long ans = 0;
+
+        // for(auto i:roads)
+        // {
+        //     long long x = 0;
+
+        //     for(auto j:i)
+        //     {
+        //         x += v[j][1];
+        //     }
+
+        //     // cout<<x<<endl;
+        //     ans += x;
+        // }
+
+        // return ans;
     }
 };
