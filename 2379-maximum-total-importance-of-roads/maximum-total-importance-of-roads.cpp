83385c51f209712a049f35b30a2ab9a0b890edda
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2024-06-28 13:20:49 +0530

    Time: 516 ms (11.04%) | Memory: 177.5 MB (14.66%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) 
+    {
+        vector<int> o_d(n,0);
+
+        for(auto i:roads)
+        {
+            for(auto j:i)
+            {
+                o_d[j]++;
+            }
+        }
+
+        vector<vector<int>> v;
+
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({o_d[i],i});
+        }
+
+        sort(v.begin(),v.end());
+
+        for(int i=0;i<n;i++)
+        {
+            v[i][0] = i+1;
+            swap(v[i][0],v[i][1]);
+            // cout<<v[i][0]<<" "<<v[i][1]<<endl;
+        }
+
+        sort(v.begin(),v.end());
+
+        long long ans = 0;
+
+        for(auto i:roads)
+        {
+            long long x = 0;
+
+            for(auto j:i)
+            {
+                x += v[j][1];
+            }
+
+            // cout<<x<<endl;
+            ans += x;
+        }
+
+        return ans;
+    }
+};
