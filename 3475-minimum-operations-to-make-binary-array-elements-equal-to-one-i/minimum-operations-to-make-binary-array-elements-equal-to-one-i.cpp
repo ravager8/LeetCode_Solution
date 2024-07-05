c916e1309a9ab3b1c922e315b6e6c787dc036162
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2024-07-05 22:19:11 +0530

    Time: 106 ms (77.15%) | Memory: 138.4 MB (62.97%) - LeetSync

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int ans = 0;
+    
+        for (int i = 0; i <= n - 3; i++) 
+        {
+            if (nums[i] == 0) 
+            {
+                ans++;
+                for (int j = 0; j < 3; j++) 
+                {
+                    nums[i + j] = 1 - nums[i + j];
+                }
+            }
+        }
+    
+        for (int i = 0; i < n; ++i) 
+        {
+            if (nums[i] == 0) 
+            {
+                return -1;
+            }
+       }
+    
+        return ans;
+    }
+
+};
