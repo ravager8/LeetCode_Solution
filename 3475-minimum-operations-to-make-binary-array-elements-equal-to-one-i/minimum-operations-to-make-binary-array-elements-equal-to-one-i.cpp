d82609459a9130ab98885868eb78959ae036ad6a
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2024-07-06 12:50:54 +0530

    Time: 108 ms (70.66%) | Memory: 138.3 MB (62.96%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -5,26 +5,24 @@ public:
         int n = nums.size();
         int ans = 0;
     
-        for (int i = 0; i <= n - 3; i++) 
+        for(int i=0;i<n-2;i++)
         {
-            if (nums[i] == 0) 
+            if(nums[i]==0)
             {
+                nums[i] =1;
+
+                nums[i+1] = 1 - nums[i+1];
+                nums[i+2] = 1 - nums[i+2];
                 ans++;
-                for (int j = 0; j < 3; j++) 
-                {
-                    nums[i + j] = 1 - nums[i + j];
-                }
             }
         }
-    
-        for (int i = 0; i < n; ++i) 
+
+        for(int i=0;i<n;i++)
         {
-            if (nums[i] == 0) 
-            {
-                return -1;
-            }
-       }
-    
+            if(nums[i]==0)
+            return -1;
+        }
+
         return ans;
     }
 
