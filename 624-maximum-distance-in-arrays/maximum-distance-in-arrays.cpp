6b1992274857df33da567d05eb6dea92cc8a5e2f
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2024-08-16 13:16:33 +0530

    Time: 232 ms (55.19%) | Memory: 107.7 MB (69.81%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -4,17 +4,17 @@ public:
 
         int n = arrays.size();
         int ans = -1e9;
-        int n1 = arrays[0].size();
-        int mx = arrays[0][n1-1];
+        // int n1 = arrays[0].size();
+        int mx = arrays[0].back();
         int mn = arrays[0][0];
 
         for(int i=1;i<n;i++){
 
-            int n2 = arrays[i].size();
+            // int n2 = arrays[i].size();
             int a = arrays[i][0];
-            int b = arrays[i][n2-1];
+            int b = arrays[i].back();
 
-            ans = max(ans,max(abs(mx-a),max(abs(mx-b),max(abs(mn-a),abs(mn-b)))));
+            ans = max(ans,max(abs(mx-a),abs(mn-b)));
 
             if(mx<b) mx = b;
             if(mn>a) mn = a;
