f797ad1d7c6cf581a3290635aa1b4a90466a82d1
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2024-08-16 13:53:25 +0530

    Time: 12 ms (23.22%) | Memory: 44.6 MB (98.07%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,12 +1,16 @@
 class Solution {
-    public int singleNumber(int[] nums) 
-    {
+    public int singleNumber(int[] nums) {
         int n = nums.length;
         int ans = 0;
+        Map<Integer, Integer> freq = new HashMap<>();
 
-        for(int i=0;i<n;i++)
-        {
-            ans = ans ^ nums[i];
+        for (int i = 0; i < n; i++) {
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() == 1)
+                return entry.getKey();
         }
 
         return ans;
