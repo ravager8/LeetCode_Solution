2a8a2777c7117f7a2e770db67f45abb6f49ac3f2
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2024-11-13 00:29:09 +0530

    Time: 48 ms (36.17%) | Memory: 29.9 MB (22.55%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string compressedString(string word) {
+
+        int n = word.size();
+        string ans = "";
+        int len = 1;
+        char prev = word[0];
+
+        for(int i=1;i<n;i++){
+            if(word[i]!=prev or len==9){
+                ans += to_string(len);
+                ans += prev;
+                len = 1;
+                prev = word[i];
+            } else{
+                len++;
+            }
+        }
+
+        ans += to_string(len);
+        ans += prev;
+
+        return ans;
+        
+    }
+};
