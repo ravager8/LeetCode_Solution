bfe2f9257e299d987fa31778432dd55ed42d9c66
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2026-02-15 00:52:56 +0530

    Time: 3 ms (76.33%) | Memory: 48.5 MB (21.20%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        
+        double[][] pyramid = new double[101][101];
+        pyramid[0][0] = (double) poured;
+
+        for(int i=0;i<=query_row;i++){
+            for(int j=0;j<=i;j++){
+                if(pyramid[i][j]>1.0){
+                    double extra = pyramid[i][j]-1.0;
+                    pyramid[i][j]=1.0;
+                    pyramid[i+1][j] += extra/2.0;
+                    pyramid[i+1][j+1] += extra/2.0;
+                }
+            }
+        }
+
+        return pyramid[query_row][query_glass];
+    }
+}
+
+
+
+// 1st row -> 1 cups -> a[i]
+// 2nd row -> 3 cups -> a[i-1] + 2 * a[i]
+// 3rd row full and 4th row 2 glass half filled -> 7 cups -> a[i-1] * 2 a[i-1]; for this row to fill all glass.  
+// same pattern should follow
