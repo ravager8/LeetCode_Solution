fae17d16dc5c57adb9d4e503eb68ba37ec767429
Sugam Sharma <96646804+ravager8@users.noreply.github.com>
2026-04-19 16:20:53 +0530

    Time: 3 ms (98.64%) | Memory: 90.2 MB (39.32%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -1,31 +1,48 @@
 class Solution {
-    public int binarySearch(int start, int end,int target, int[] nums2){
-        int curr = start-1;
+    // public int binarySearch(int start, int end,int target, int[] nums2){
+    //     int curr = start-1;
 
-        while(start<=end){
-            int mid = start + (end - start) / 2;
+    //     while(start<=end){
+    //         int mid = start + (end - start) / 2;
 
-            if(nums2[mid]>=target) {
-                start = mid+1;
-                curr = mid;
-            }
-            else end = mid - 1;
-        }
+    //         if(nums2[mid]>=target) {
+    //             start = mid+1;
+    //             curr = mid;
+    //         }
+    //         else end = mid - 1;
+    //     }
 
-        return curr;
-    }
+    //     return curr;
+    // }
 
     public int maxDistance(int[] nums1, int[] nums2) {
+        // int ans = 0;
+        // int curr = 0;
+        // int n = nums1.length;
+        // int m = nums2.length;
+
+        // for(int i=0;i<n && i<m;i++){
+        //     curr = binarySearch(i,m-1,nums1[i],nums2);
+        //     ans = Math.max(ans,curr-i);
+        // }
+
+        // return ans;
+
         int ans = 0;
-        int curr = 0;
         int n = nums1.length;
         int m = nums2.length;
+        int i = 0;
+        int j = 0;
 
-        for(int i=0;i<n && i<m;i++){
-            curr = binarySearch(i,m-1,nums1[i],nums2);
-            ans = Math.max(ans,curr-i);
+        while(i<n && j<m){
+            if(nums1[i]<=nums2[j]){
+                ans = Math.max(ans,j-i);
+                j++;
+            } else {
+                j++; 
+                i++;
+            }
         }
-
         return ans;
     }
 }
